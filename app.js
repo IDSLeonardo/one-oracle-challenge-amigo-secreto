7ea3e41759fcdaf7ad1d83f32a7ad333e30ed517
Leonardo Silva <leonardo_izaias@outlook.com>
2025-09-14 02:32:03 -0300

    feat: adicionar lógica completa do Amigo Secreto (adicionar nomes e sortear sem repetição)

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomeAmigos = [];
+let amigosJaSorteados = [];
+
+function adicionarAmigo() {
+  let nome = document.querySelector("#amigo").value;
+  if (nome === "") {
+    alert("Por favor, digite um nome válido!");
+    return;
+  }
+  nomeAmigos.push(nome);
+  let lista = document.querySelector("#listaAmigos");
+  lista.innerHTML = nomeAmigos;
+  document.querySelector("#amigo").value = "";
+}
+
+function sortearAmigo() {
+  if (nomeAmigos.length >= 3) {
+    if (amigosJaSorteados.length === nomeAmigos.length) {
+        alert('Todos os amigos já foram sorteados');
+        return;
+    }
+    let indiceSorteado = parseInt(Math.random() * nomeAmigos.length);
+    let amigoSecreto = nomeAmigos[indiceSorteado];
+    while (amigosJaSorteados.includes(amigoSecreto)) {
+        indiceSorteado = parseInt(Math.random() * nomeAmigos.length);
+        amigoSecreto = nomeAmigos[indiceSorteado];
+    }
+    amigosJaSorteados.push(amigoSecreto);
+    document.querySelector("#resultado").innerHTML = `O amigo secreto é ${amigoSecreto}`;
+  } else {
+    alert("Você deve digitar pelo menos três nomes para sortear os amigos!");
+  }
+}
